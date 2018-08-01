f910949e198c627c67b72c598339695fd1b4c3a8
Alexander Shashkevich <alex@stunpix.com>
2018-08-01 16:30:57 +0300

    Integrate and fix keyboard shortcuts

--- wme-hn2poi.user.js
+++ wme-hn2poi.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         WME HN2POI
-// @version      2018.07.20.002
+// @version      2018.08.01.001
 // @description  Converts HouseNumbers to POI
 // @author       turbopirate
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
 // @grant        none
 // @namespace https://greasyfork.org/users/166361
-// @require https://greasyfork.org/scripts/16071-wme-keyboard-shortcuts/code/WME%20Keyboard%20Shortcuts.js
 // ==/UserScript==
 
 (function() {
@@ -90,12 +89,12 @@
     
     const scriptName = 'hn2poi';
 
-    WMEKSRegisterKeyboardShortcut(scriptName, 'HN2POI', 'hn-to-poi', txt('makePoiButtonText'), makePOI, '-1');
-    WMEKSRegisterKeyboardShortcut(scriptName, 'HN2POI', 'delete-hn', txt('delHNButtonText'), delHN, '-1');
-    WMEKSLoadKeyboardShortcuts(scriptName);
+    RegisterKeyboardShortcut(scriptName, 'HN2POI', 'hn-to-poi', txt('makePoiButtonText'), makePOI, '-1');
+    RegisterKeyboardShortcut(scriptName, 'HN2POI', 'delete-hn', txt('delHNButtonText'), delHN, '-1');
+    LoadKeyboardShortcuts(scriptName);
 
     window.addEventListener("beforeunload", function() {
-        WMEKSSaveKeyboardShortcuts(scriptName);
+        SaveKeyboardShortcuts(scriptName);
     }, false);
   }
 
@@ -236,6 +235,115 @@
       );
     });
   }
-  
+
+  //setup keyboard shortcut's header and add a keyboard shortcuts
+  function RegisterKeyboardShortcut(ScriptName, ShortcutsHeader, NewShortcut, ShortcutDescription, FunctionToCall, ShortcutKeysObj) {
+    // Figure out what language we are using
+    var language = I18n.currentLocale();
+    //check for and add keyboard shourt group to WME
+    try {
+        var x = I18n.translations[language].keyboard_shortcuts.groups[ScriptName].members.length;
+    } catch (e) {
+        //setup keyboard shortcut's header
+        W.accelerators.Groups[ScriptName] = []; //setup your shortcut group
+        W.accelerators.Groups[ScriptName].members = []; //set up the members of your group
+        I18n.translations[language].keyboard_shortcuts.groups[ScriptName] = []; //setup the shortcuts text
+        I18n.translations[language].keyboard_shortcuts.groups[ScriptName].description = ShortcutsHeader; //Scripts header
+        I18n.translations[language].keyboard_shortcuts.groups[ScriptName].members = []; //setup the shortcuts text
+    }
+    //check if the function we plan on calling exists
+    if (FunctionToCall && (typeof FunctionToCall == "function")) {
+        I18n.translations[language].keyboard_shortcuts.groups[ScriptName].members[NewShortcut] = ShortcutDescription; //shortcut's text
+        W.accelerators.addAction(NewShortcut, {
+            group: ScriptName
+        }); //add shortcut one to the group
+        //clear the short cut other wise the previous shortcut will be reset MWE seems to keep it stored
+        var ClearShortcut = '-1';
+        var ShortcutRegisterObj = {};
+        ShortcutRegisterObj[ClearShortcut] = NewShortcut;
+        W.accelerators._registerShortcuts(ShortcutRegisterObj);
+        if (ShortcutKeysObj !== null) {
+            //add the new shortcut
+            ShortcutRegisterObj = {};
+            ShortcutRegisterObj[ShortcutKeysObj] = NewShortcut;
+            W.accelerators._registerShortcuts(ShortcutRegisterObj);
+        }
+        //listen for the shortcut to happen and run a function
+        W.accelerators.events.register(NewShortcut, null, function() {
+            FunctionToCall();
+        });
+    } else {
+        alert('The function ' + FunctionToCall + ' has not been declared');
+    }
+
+  }
+  //if saved load and set the shortcuts
+  function LoadKeyboardShortcuts(ScriptName) {
+	if (localStorage[ScriptName + 'KBS']) {
+		var LoadedKBS = JSON.parse(localStorage[ScriptName + 'KBS']); //JSON.parse(localStorage['WMEAwesomeKBS']);
+		for (var i = 0; i < LoadedKBS.length; i++) {
+			W.accelerators._registerShortcuts(LoadedKBS[i]);
+		}
+	}
+  }
+
+  function SaveKeyboardShortcuts(ScriptName) {
+	var TempToSave = [];
+	for (var name in W.accelerators.Actions) {
+		var TempKeys = "";
+		if (W.accelerators.Actions[name].group == ScriptName) {
+			if (W.accelerators.Actions[name].shortcut) {
+				if (W.accelerators.Actions[name].shortcut.altKey === true) {
+					TempKeys += 'A';
+				}
+				if (W.accelerators.Actions[name].shortcut.shiftKey === true) {
+					TempKeys += 'S';
+				}
+				if (W.accelerators.Actions[name].shortcut.ctrlKey === true) {
+					TempKeys += 'C';
+				}
+				if (TempKeys !== "") {
+					TempKeys += '+';
+				}
+				if (W.accelerators.Actions[name].shortcut.keyCode) {
+					TempKeys += W.accelerators.Actions[name].shortcut.keyCode;
+				}
+			} else {
+				TempKeys = "-1";
+			}
+			var ShortcutRegisterObj = {};
+			ShortcutRegisterObj[TempKeys] = W.accelerators.Actions[name].id;
+			TempToSave[TempToSave.length] = ShortcutRegisterObj;
+		}
+	}
+	localStorage[ScriptName + 'KBS'] = JSON.stringify(TempToSave);
+  }
+
+/// Examples
+
+/*
+//add two short cuts
+WMERegisterKeyboardShortcut('WMEAwesome', 'WME Awesome Script', 'AwesomeShortcut1', 'Awesome Descrption 1', WMEKyboardShortcutToCall, '-1'); //shortcut1
+WMERegisterKeyboardShortcut('WMEAwesome', 'WME Awesome Script',	'AwesomeShortcut2', 'Awesome Descrption 2', WMEKyboardShortcutToCall, '-1'); //shortcut1
+WMERegisterKeyboardShortcut('WMEAwesome', 'WME Awesome Script',	'AwesomeShortcut3', 'Awesome Descrption 3', WMEKyboardShortcutToCall, 'ASC+l'); //shortcut1
+//WMERegisterKeyboardShortcut('WMEAwesome','AwesomeShortcut2','Awesome Descrption 2',doesnotexist,'-1'); //fuction does not exist
+
+
+//load the saved shortcuts
+WMELoadKeyboardShortcuts('WMEAwesome');
+
+//before unloading WME save the shortcuts
+window.addEventListener("beforeunload", function() {
+	WMESaveKeyboardShortcuts('WMEAwesome');
+}, false);
+
+//displays all of the shortcuts in the console
+//W.accelerators.Actions;
+
+//saved shortcuts to console
+//JSON.parse(localStorage['WMEAwesomeKBS']);
+
+*/  
+
   wait();
 })();
