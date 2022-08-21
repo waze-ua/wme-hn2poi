98aeecbfdaef4db8855a63d9ddc24f5a56c64655
SapozhnikUA <48523324+SapozhnikUA@users.noreply.github.com>
2022-08-21 20:53:37 +0300

    Update wme-hn2poi.user.js
    change version

--- wme-hn2poi.user.js
+++ wme-hn2poi.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME HN2POI
-// @version      2021.09.13.01
+// @version      2022.08.21.01
 // @description  Converts HouseNumbers to POI
 // @author       turbopirate, Andrei Pavlenko
 // @include      /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
